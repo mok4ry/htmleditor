8aeb2a0bf1b6821844c4878484f736120984b8db
Matt Mokary <mxm6060@rit.edu>
2013-03-21 17:24:28 -0400

    Initial Maven project structure

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ htmleditor
 ==========
 
 HTMLEditor project for SE362 - Engineering of Software Subsystems.
+
+Team members:
+*Adam Blaine
+*Obaseki Idemudia
+*Matt Mokary
+*Zach Nielson
+*Wayne Starr
---
+++ src/main/java/edu/rit/se/reichmafia/htmleditor/HTMLEditor.java
@@ -0,0 +1,13 @@
+package edu.rit.se.reichmafia.htmleditor;
+
+/**
+ * Hello world!
+ *
+*/
+public class HTMLEditor 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
