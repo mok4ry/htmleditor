ed397c28c5104e44cb9e0cd8365a551e4ca308b5
mok4ry <mxm6060@rit.edu>
2013-03-24 23:11:26 -0400

    GUI integration, stub for controller

--- src/main/java/edu/rit/se/reichmafia/htmleditor/HTMLEditor.java
+++ src/main/java/edu/rit/se/reichmafia/htmleditor/HTMLEditor.java
@@ -1,5 +1,7 @@
 package edu.rit.se.reichmafia.htmleditor;
 
+import edu.rit.se.reichmafia.htmleditor.views.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class HTMLEditor
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MainScreen ms = new MainScreen();
+        ms.setVisible(true);
     }
 }
---
+++ src/main/java/edu/rit/se/reichmafia/htmleditor/controllers/MainController.java
@@ -0,0 +1,11 @@
+package edu.rit.se.reichmafia.htmleditor.controllers;
+
+/**
+ * Main controller for managing information between the HTML Editor's view and
+ * models.
+ * 
+ * @author Team ReichMafia
+ */
+public class MainController {
+    
+}
