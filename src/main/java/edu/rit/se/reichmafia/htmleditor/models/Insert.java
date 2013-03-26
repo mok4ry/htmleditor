ca1a9a97cc8e0d10663b5f3f8c8cdad18afc6865
Wayne E Starr <Wayne E Starr@129.21.41.0>
2013-03-26 14:58:24 -0400

    Added algorithms to insert for table and layered tags

--- src/main/java/edu/rit/se/reichmafia/htmleditor/models/Insert.java
+++ src/main/java/edu/rit/se/reichmafia/htmleditor/models/Insert.java
@@ -11,8 +11,33 @@ package edu.rit.se.reichmafia.htmleditor.models;
  */
 public class Insert {
     
-    public String insert (String text, String name, int index) {
-        String newString = text.substring(0,index) + "<" + name + "></" + name + ">" + text.substring(index+1,text.length()-1);
+    public String insertFlat (String text, String name, int index) {
+        String newString = text.substring(0,index) + "<" + name + "></" + name
+                + ">" + text.substring(index+1,text.length()-1);
+        return newString;
+    }
+    
+    public String insertLayered (String text, String name, String subName, 
+            int index, int subTags) {
+        String newString = text.substring(0,index) + "\n<" + name + ">\n";
+        int i = 0;
+        for (i = 0; i < subTags; i++) {
+            newString = newString + insertFlat("", subName, 0) + "\n";
+        }
+        newString = newString + "</" + name + ">\n" + text.substring(index+1,
+                text.length()-1);
+        return newString;
+    }
+    
+    public String insertTable (String text, int index, int rows, int cols) {
+        String newString = text.substring(0,index) + "\n<table>\n";
+        newString = newString + insertLayered("", "th", "td", 0, cols);
+        int i = 0;
+        for (i = 0; i < rows-1; i++) {
+            newString = newString + insertLayered("", "tr", "td", 0, cols);
+        }
+        newString = newString + "</table>\n" + text.substring(index+1,
+                text.length()-1);
         return newString;
     }
 }
