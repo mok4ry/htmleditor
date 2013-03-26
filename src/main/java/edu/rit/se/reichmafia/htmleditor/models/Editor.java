0c9ddfa243bc8d498cd2c96be713efd6e00ecf6e
Wayne E Starr <Wayne E Starr@129.21.41.0>
2013-03-26 15:10:50 -0400

    Updated editor to reflect changes to editor

--- src/main/java/edu/rit/se/reichmafia/htmleditor/models/Editor.java
+++ src/main/java/edu/rit/se/reichmafia/htmleditor/models/Editor.java
@@ -22,11 +22,23 @@ public class Editor {
         String oldString = toIndent.getText();
         String newText = toIndent.getText().substring(startIndex,endIndex);
         newText = indentor.indentText(newText);
-        toIndent.setText(oldString.substring(0,startIndex-1) + newText + oldString.substring(endIndex+1,oldString.length()));
+        toIndent.setText(oldString.substring(0,startIndex-1) + newText + 
+                oldString.substring(endIndex+1,oldString.length()));
     }
     
     public void insert (Buffer toInsert, String name, int startIndex) {
-        toInsert.setText(inserter.insert(toInsert.getText(),name,startIndex));
+        toInsert.setText(inserter.insertFlat(toInsert.getText(), name, 
+                startIndex));
     }
     
+    public void insert (Buffer toInsert, String name, String subName, 
+            int startIndex, int subTags) {
+        toInsert.setText(inserter.insertLayered(toInsert.getText(), name, 
+                subName, startIndex, subTags));
+    }
+    
+    public void insert (Buffer toInsert, int startIndex, int rows, int cols) {
+        toInsert.setText(inserter.insertTable(toInsert.getText(), startIndex, 
+                rows, cols));
+    }
 }
