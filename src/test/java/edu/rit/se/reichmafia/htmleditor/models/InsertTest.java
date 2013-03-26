316cd834b78152dcd2ea7bd2a120ff925a4445fa
Wayne E Starr <Wayne E Starr@129.21.41.0>
2013-03-26 18:51:32 -0400

    Tests flushed out

--- src/test/java/edu/rit/se/reichmafia/htmleditor/models/InsertTest.java
+++ src/test/java/edu/rit/se/reichmafia/htmleditor/models/InsertTest.java
@@ -4,6 +4,7 @@
  */
 package edu.rit.se.reichmafia.htmleditor.models;
 
+import static junit.framework.Assert.assertEquals;
 import junit.framework.TestCase;
 
 /**
@@ -27,48 +28,88 @@ public class InsertTest extends TestCase {
     }
 
     /**
-     * Test of insertFlat method, of class Insert.
+     * Test of insertFlat method when text is empty.
      */
-    public void testInsertFlat() {
+    public void testInsertFlatEmpty() {
         System.out.println("insertFlat");
-        String text = "";
-        String name = "em";
-        int index = 0;
         Insert instance = new Insert();
         String expResult = "<em></em>";
-        String result = instance.insertFlat(text, name, index);
+        String result = instance.insertFlat("", "em", 0);
         assertEquals(expResult, result);
     }
-
+    
     /**
-     * Test of insertLayered method, of class Insert.
+     * Test when the tags are inserted between existing text
      */
-    public void testInsertLayered() {
-        System.out.println("insertLayered");
-        String text = "";
-        String name = "ul";
-        String subName = "li";
-        int index = 0;
-        int subTags = 0;
+    public void testInsertFlatMiddle() {
         Insert instance = new Insert();
-        String expResult = "<ul>\n<li></li>\n</ul>\n";
-        String result = instance.insertLayered(text, name, subName, index, subTags);
-        System.out.println(result);
+        String expResult = "Hel<b></b>lo";
+        String result = instance.insertFlat("Hello", "b", 3);
         assertEquals(expResult, result);
     }
 
     /**
-     * Test of insertTable method, of class Insert.
+     * Test of insertLayered method with 0 subTags.
      */
-    public void testInsertTable() {
+    public void testInsertLayeredZero() {
+        System.out.println("insertLayered");
+        Insert instance = new Insert();
+        String expResult = "<ul>\n</ul>\n";
+        String result = instance.insertLayered("", "ul", "li", 0, 0);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of insertLayered method on Non-Empty text with 3 subTags.
+     */
+    public void testInsertLayeredFive() {
+        Insert instance = new Insert();
+        String expResult = "Hel<ul>\n<li></li>\n<li></li>\n<li></li>\n</ul>\nlo";
+        String result = instance.insertLayered("Hello", "ul", "li", 3, 3);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of insertTable method with 0 rows and 0 columns.
+     */
+    public void testInsertTableZeroZero() {
         System.out.println("insertTable");
-        String text = "";
-        int index = 0;
-        int rows = 0;
-        int cols = 0;
         Insert instance = new Insert();
         String expResult = "\n<table>\n</table>\n";
-        String result = instance.insertTable(text, index, rows, cols);
+        String result = instance.insertTable("", 0, 0, 0);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of insertTable method with 1 rows and 0 columns.
+     */
+    public void testInsertTableOneZero() {
+        Insert instance = new Insert();
+        String expResult = "\n<table>\n<th>\n</th>\n</table>\n";
+        String result = instance.insertTable("", 0, 1, 0);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of insertTable method with 1 rows and 3 columns.
+     */
+    public void testInsertTableOneThree() {
+        Insert instance = new Insert();
+        String expResult = "\n<table>\n<th>\n<td></td>\n<td></td>\n<td></td>\n"
+                + "</th>\n</table>\n";
+        String result = instance.insertTable("", 0, 1, 3);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of insertTable method with 3 rows and 3 columns.
+     */
+    public void testInsertTableThreeThree() {
+        Insert instance = new Insert();
+        String expResult = "\n<table>\n<th>\n<td></td>\n<td></td>\n<td></td>\n"
+                + "</th>\n<tr>\n<td></td>\n<td></td>\n<td></td>\n</tr>\n"
+                + "<tr>\n<td></td>\n<td></td>\n<td></td>\n</tr>\n</table>\n";
+        String result = instance.insertTable("", 0, 3, 3);
         assertEquals(expResult, result);
     }
 }
