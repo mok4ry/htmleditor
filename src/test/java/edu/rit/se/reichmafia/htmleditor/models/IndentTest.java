3d683ffa19cb3118c969aa6e406ec0822127343f
Wayne E Starr <Wayne E Starr@129.21.41.0>
2013-03-27 19:25:02 -0400

    Tests on indent mostly complete. Indent fixed. Editor documented.

--- src/test/java/edu/rit/se/reichmafia/htmleditor/models/IndentTest.java
+++ src/test/java/edu/rit/se/reichmafia/htmleditor/models/IndentTest.java
@@ -4,6 +4,7 @@
  */
 package edu.rit.se.reichmafia.htmleditor.models;
 
+import static junit.framework.Assert.assertEquals;
 import junit.framework.TestCase;
 
 /**
@@ -30,10 +31,49 @@ public class IndentTest extends TestCase {
      * Test of indentText method with empty text.
      */
     public void testIndentTextEmpty() {
-        System.out.println("indentText");
+        System.out.println("indentTextEmpty");
         Indent instance = new Indent();
         String expResult = "    \n\n";
-        String result = instance.indentText("");
+        String result = instance.indentText("", 0, 0);
+        System.out.println(result);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of indentText method with plain text.
+     */
+    public void testIndentTextPlain() {
+        System.out.println("indentTextPlain");
+        Indent instance = new Indent();
+        String input = "Hello World!\nI love the world!\n";
+        String expResult = "    \n\n";
+        String result = instance.indentText(input, 0, input.length()-1);
+        System.out.println(result);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of indentText method with basic html.
+     */
+    public void testIndentTextBasic() {
+        System.out.println("indentTextBasic");
+        Indent instance = new Indent();
+        String expResult = "<b>Hello</b>\n<em>World</em>";
+        String input = "<b>Hello</b>\n<em>World</em>";
+        String result = instance.indentText(input, 0, input.length()-1);
+        System.out.println(result);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of indentText method with advanced html.
+     */
+    public void testIndentTextAdvanced() {
+        System.out.println("indentTextAdvanced");
+        Indent instance = new Indent();
+        String expResult = "<table>\n<th>\n<tr><b>Hello</b></tr>\n</th>\n</table>\n";
+        String input = "<table>\n<th>\n<tr><b>Hello</b></tr>\n</th>\n</table>\n\n\n";
+        String result = instance.indentText(input, 0, input.length()-1);
         System.out.println(result);
         assertEquals(expResult, result);
     }
