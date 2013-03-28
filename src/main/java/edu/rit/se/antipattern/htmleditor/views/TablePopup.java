44427b3099fa67a422cbab8f1879fefe723d6a73
Wayne E Starr <Wayne E Starr@129.21.41.0>
2013-03-28 19:32:24 -0400

    Table text boxes fixed

--- src/main/java/edu/rit/se/antipattern/htmleditor/views/TablePopup.java
+++ src/main/java/edu/rit/se/antipattern/htmleditor/views/TablePopup.java
@@ -95,7 +95,7 @@ public class TablePopup extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        cont.insert(buffer, curs, Integer.parseInt(jTextField1.getText()), Integer.parseInt(jTextField1.getText()));
+        cont.insert(buffer, curs, Integer.parseInt(jTextField1.getText()), Integer.parseInt(jTextField2.getText()));
         main.updateText();
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
